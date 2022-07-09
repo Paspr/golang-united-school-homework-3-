10e9861c7238d6fbb06196556bcc6f351fd5164a
Pavel Osminin <osperevod@gmail.com>
2022-07-09 17:49:26 +0500

    initial version of 'map' task

--- task03-map.go
+++ task03-map.go
@@ -1,6 +1,23 @@
 package homework
 
+import (
+	"sort"
+)
+
 func sortMapValues(input map[int]string) (result []string) {
-	//Place your code here
+
+	// new slice for keys of input map
+	keys := make([]int, 0, len(input))
+	// populate the slice by keys
+	for k := range input {
+		keys = append(keys, k)
+	}
+
+	sort.Ints(keys)
+	// populate the result slice by map values in the ascending key order
+	for _, k := range keys {
+		result = append(result, input[k])
+	}
+
 	return
 }
