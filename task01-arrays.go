b57eb31949dee0cc688350f40f50b496d4367f2f
Pavel Osminin <osperevod@gmail.com>
2022-07-09 16:31:51 +0500

    initial version of 'arrays' task

--- task01-arrays.go
+++ task01-arrays.go
@@ -1,6 +1,10 @@
 package homework
 
 func average(input [15]float32) (result float32) {
-	//Place your code here
+	var sum float32 = 0.0
+	for _, element := range input {
+		sum = sum + element
+	}
+	result = sum / float32(len(input))
 	return
 }
